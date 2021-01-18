b82064e700ffaa1dd643d926b5c23d028d492267
pixelomer <37103802+pixelomer@users.noreply.github.com>
2021-01-18 14:17:27 +0300

    Fixed .gitignore

---
+++ bad-apple/video.h
@@ -0,0 +1,6 @@
+#include "pongo/pongo.h"
+uint32_t bad_apple_frame_count;
+uint32_t bad_apple_frame_width;
+uint32_t bad_apple_frame_height;
+size_t bad_apple_decompressed_size;
+uint8_t bad_apple_compressed[93322653];
